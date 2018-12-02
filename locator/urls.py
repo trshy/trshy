7b16efb3df4a28a79ec4ab49d3ec669cf8097c84
Michael Alperovich <misha.alperovich1@gmail.com>
2018-12-01 22:58:23 -0500

    Basic working map

--- locator/urls.py
+++ locator/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls import url
 from .views import (
     index_view,
     profile_view,
+    map_view,
 )
 
 urlpatterns = [
     url(r'^$', index_view, name='index'),
     url(r'^profile/$', profile_view, name='profile'),
-    url(r'^map/$', profile_view, name='map'),
-    url(r'^trashcan(?P<id>[\d-]+)/$', profile_view, name='trashcan'),
-
+    url(r'^map/$', map_view, name='map'),
 ]
