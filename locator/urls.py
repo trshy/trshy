e61ebc8e1b391d4ad1f213d0bb36559801d5eb63
Phillip T <donutdeflector@protonmail.com>
2018-12-02 05:35:58 -0500

    fixes

--- locator/urls.py
+++ locator/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^$', index_view, name='index'),
     url(r'^profile/$', profile_view, name='profile'),
     url(r'^map/$', map_view, name='map'),
-    url(r'^map/add', add_view, name='map'),
+    url(r'^map/add', add_view, name='add'),
     url(r'^rate', rate_view, name='rate'),
 
 ]
