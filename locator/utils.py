f66bfc6fde6e5732c268a5458228383d1c20535b
Michael Alperovich <misha.alperovich1@gmail.com>
2018-12-02 00:53:35 -0500

    added clustering

---
+++ locator/utils.py
@@ -0,0 +1,19 @@
+import csv
+
+from locator.models import Trashcan
+
+
+def csv_import():
+    with open('Downtown_Trash_Receptacles.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        cnt = 0
+        for row in reader:
+            if cnt > 1:
+                trashcan = Trashcan()
+                coords = row[0].split(",")
+                trashcan.longitude = float(coords[0])
+                trashcan.latitude = float(coords[1])
+                trashcan.save()
+            cnt += 1
+
+
